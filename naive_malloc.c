ba42180af317f7384dea457934576f61ddca7008
Ran Kong <rkbriankong@gmail.com>
2021-11-19 11:11:53 -0600

    test naive

--- naive_malloc.c
+++ naive_malloc.c
@@ -8,5 +8,33 @@
  */
 void *naive_malloc(size_t size)
 {
-	;
+	size_t chunk_hdr = aligner(size + sizeof(size_t));
+	static size_t new_alloc;
+	void *ret_ptr;
+	static void *headptr;
+	static long page_size;
+
+	if (!size)
+		return (NULL);
+	while (new_alloc < chunk_hdr)
+	{
+		if (headptr == NULL)
+			page_size = sysconf(_SC_PAGESIZE), headptr = sbrk(page_size);
+		else
+			sbrk(page_size);
+		new_alloc = new_alloc + page_size;
+	}
+	ret_ptr = headptr, memcpy(ret_ptr, &chunk_hdr, sizeof(chunk_hdr));
+	headptr = (void *)((char *)(ret_ptr) + chunk_hdr);
+	return ((void *)((char *)(ret_ptr) + aligner(sizeof(chunk_hdr))));
+}
+
+/**
+ * aligner - Linux uses 8-byte alignment
+ * @size: size before align
+ * Return: aligned value 
+ */
+size_t aligner(size_t size)
+{
+	return ((8 - 1 + size) & ~(8 - 1)); /* masking the  */
 }
